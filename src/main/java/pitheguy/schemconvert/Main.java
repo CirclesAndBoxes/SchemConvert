fbca669bf1cfaa3ab240968e6f70a55994b00a79
PiTheGuy <the4mooresrock@gmail.com>
2025-03-16 13:55:45 -0500

    CLI improvements

--- src/main/java/pitheguy/schemconvert/Main.java
+++ src/main/java/pitheguy/schemconvert/Main.java
@@ -1,53 +1,106 @@
 package pitheguy.schemconvert;
 
-import joptsimple.OptionParser;
-import joptsimple.OptionSet;
+import joptsimple.*;
 import pitheguy.schemconvert.converter.ConversionException;
 import pitheguy.schemconvert.converter.Converter;
 import pitheguy.schemconvert.converter.formats.SchematicFormat;
 import pitheguy.schemconvert.converter.formats.SchematicFormats;
 import pitheguy.schemconvert.ui.Gui;
+import pitheguy.schemconvert.util.Util;
 
 import java.io.*;
 
 public class Main {
-    public static boolean isCommandLine = false;
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         if (args.length == 0) new Gui();
-        else {
-            isCommandLine = true;
-            OptionParser parser = new OptionParser();
-            parser.accepts("help", "Show this help message").forHelp();
-            parser.accepts("input", "Input file").withRequiredArg().ofType(File.class).required();
-            parser.accepts("output", "Output file").withRequiredArg().ofType(File.class).required();
-            parser.accepts("format", "Output format").withRequiredArg().ofType(String.class).required();
-            OptionSet options = parser.parse(args);
-            File inputFile = (File) options.valueOf("input");
+        else processCommandLine(args);
+    }
+
+    private static void processCommandLine(String[] args) throws IOException {
+        OptionParser parser = new OptionParser();
+        parser.accepts("help", "Show this help message").forHelp();
+        parser.accepts("input", "Input file").withRequiredArg().ofType(File.class).required();
+        parser.accepts("format", "Output format (One of: nbt, schem, litematic). If not specified, format will be inferred from output file").withRequiredArg().ofType(String.class);
+        parser.accepts("output", "Output file. If not specified, will output to the same folder as the input file.").requiredUnless("format").withRequiredArg().ofType(File.class);
+        OptionSet options;
+        try {
+            options = parser.parse(args);
+        } catch (OptionException e) {
+            System.err.println(e.getMessage());
+            System.exit(1);
+            return;
+        }
+        if (options.has("help")) {
+            parser.printHelpOn(System.out);
+            return;
+        }
+        File inputFile = (File) options.valueOf("input");
+        Output output = getOutput(options);
+        File outputFile = output.file();
+        SchematicFormat format = output.format();
+        if (!inputFile.exists()) {
+            printError("Input file not found: " + inputFile);
+            return;
+        }
+        if (Converter.SCHEMATIC_EXTENSIONS.stream().noneMatch(ext -> inputFile.getName().endsWith(ext)))
+            System.err.println("Unrecognized input file: " + inputFile);
+        try {
+            new Converter().convert(inputFile, outputFile, format);
+            System.out.println("Successfully converted " + inputFile + " to " + outputFile);
+        } catch (IOException e) {
+            printError("An error occurred reading or writing to disk: " + e.getMessage());
+        } catch (ConversionException e) {
+            printError(e.getMessage());
+        } catch (Exception e) {
+            printError("An error occurred while converting " + inputFile + " to " + outputFile);
+        }
+    }
+
+    private static Output getOutput(OptionSet options) {
+        if (options.has("output") && options.has("format")) {
             File outputFile = (File) options.valueOf("output");
             String formatStr = (String) options.valueOf("format");
-            SchematicFormat format = switch (formatStr) {
-                case "nbt" -> SchematicFormats.NBT;
-                case "schem" -> SchematicFormats.SCHEM;
-                case "litematic" -> SchematicFormats.LITEMATIC;
-                default -> {
-                    System.err.println("Unrecognized output format: " + formatStr);
-                    System.exit(1);
-                    yield null; // Required by compiler
-                }
-            };
-            if (Converter.SCHEMATIC_EXTENSIONS.stream().noneMatch(ext -> inputFile.getName().endsWith(ext)))
-                System.err.println("Unrecognized input file: " + inputFile);
+            SchematicFormat format;
             try {
-                new Converter().convert(inputFile, outputFile, format);
-                System.out.println("Successfully converted " + inputFile + " to " + outputFile);
-            } catch (ConversionException e) {
-                System.err.println(e.getMessage());
-                System.exit(1);
-            } catch (Exception e) {
-                System.err.println("An error occurred while converting " + inputFile + " to " + outputFile);
-                System.exit(1);
+                format = SchematicFormats.formatFromExtension("." + formatStr);
+            } catch (IllegalArgumentException e) {
+                printError("Unrecognized output format: " + formatStr);
+                return null;
             }
+            if (!Util.getExtension(outputFile.getName()).equalsIgnoreCase(format.getExtension()))
+                System.out.println("Warning: output file " + outputFile + " does not match the format " + format.getExtension());
+            return new Output((File) options.valueOf("output"), format);
+        } else if (options.has("output")) {
+            File outputFile = (File) options.valueOf("output");
+            SchematicFormat format;
+            try {
+                format = SchematicFormats.formatFromExtension(Util.getExtension(outputFile.getName()));
+            } catch (IllegalArgumentException e) {
+                printError("Output file doesn't match any known format. Specify the format with -format");
+                return null;
+            }
+            return new Output(outputFile, format);
+        } else if (options.has("format")) {
+            SchematicFormat format;
+            try {
+                format = SchematicFormats.formatFromExtension("." + options.valueOf("format"));
+            } catch (IllegalArgumentException e) {
+                printError("Unrecognized output format: " + options.valueOf("format"));
+                return null;
+            }
+            File inputFile = (File) options.valueOf("input");
+            File outputFile = new File(Util.stripExtension(inputFile.getName()) + format.getExtension());
+            return new Output(outputFile, format);
+        } else {
+            throw new IllegalStateException("Missing both output and format");
         }
+    }
+
+    private static void printError(String message) {
+        System.err.println(message);
+        System.exit(1);
+    }
+
+    private record Output(File file, SchematicFormat format) {
     }
 }
--- src/main/java/pitheguy/schemconvert/converter/formats/SchematicFormats.java
+++ src/main/java/pitheguy/schemconvert/converter/formats/SchematicFormats.java
@@ -4,4 +4,13 @@ public class SchematicFormats {
     public static final SchematicFormat NBT = new NbtSchematicFormat();
     public static final SchemSchematicFormat SCHEM = new SchemSchematicFormat();
     public static final LitematicSchematicFormat LITEMATIC = new LitematicSchematicFormat();
+
+    public static SchematicFormat formatFromExtension(String extension) {
+        return switch (extension) {
+            case ".nbt" -> NBT;
+            case ".schem" -> SCHEM;
+            case ".litem" -> LITEMATIC;
+            default -> throw new IllegalArgumentException("Unknown extension: " + extension);
+        };
+    }
 }
